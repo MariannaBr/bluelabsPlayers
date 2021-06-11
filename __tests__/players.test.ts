86571107debc66232e008a769b4425a9fae489ad
marianna <marianna@primeflow.com>
2021-06-11 18:32:57 +0200

    added tests for api requests

--- __tests__/players.test.ts
+++ __tests__/players.test.ts
@@ -1,9 +1,36 @@
-import { post } from "../src/routes/players"
+import type { Request, Response } from "express"
+import { post, del, put, get } from "../src/routes/players"
 
+const request = require("supertest")
+const app = require("../src/routes/players/index")
+
+const getMockResponse = () => {
+  return {
+    data: [],
+    writeHead : function(status, header) {},
+    end: function(body) {
+      this.data.push(body);
+    }
+  }
+}
 describe("the `players` endpoint", () => {
-    it("exports a `post` function", () => {
+    it("exports a `post` function, checks `post` function if it adds to the list and if `get` gives the updated list", () => {
         expect(post).toBeDefined()
+        let req = {body: JSON.stringify({id:0})}
+        let res = getMockResponse()
+      post(req as Request, res as unknown as Response);
+        res = getMockResponse()
+      get(req as Request, res as unknown as Response)
+      expect(JSON.parse(res.data[0]).length).toEqual(6)
     })
-
+    it("exports a `delete` function", () => {
+      expect(del).toBeDefined()
+    })
+    it("exports a `put` function", () => {
+      expect(put).toBeDefined()
+    })
+    it("exports a `get` function", () => {
+      expect(get).toBeDefined()
+    })
     // Add more tests here (e.g. 'returns 200 on success or 500 for invalid requests')
 })
